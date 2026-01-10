c60d5b8265d76c52dbd539d6dc9e6eb3499fae89
Peter Dubec <peter@compositional-it.com>
2026-01-10 11:39:50 +0000

    add slight changes to container setup

--- runVNC.sh
+++ runVNC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tightvncserver :1 &
+tightvncserver :1
 sleep 0.5
-/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 &
+/usr/share/novnc/utils/novnc_proxy --listen 6080 --vnc localhost:5901 &
 tail -f /dev/null

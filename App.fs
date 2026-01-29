3d964381d9ff80de1227ace639058f07b8facb6f
Peter Dubec <peter.dubetz@gmail.com>
2026-01-29 16:14:27 +0100

    faffing around with camera permissions. doesn't work, but hey, it compiles!

--- App.fs
+++ App.fs
@@ -1,7 +1,9 @@
 namespace BookHoundApp
 
+open System.IO
 open Fabulous
 open Fabulous.Maui
+open Fabulous.Maui.PathBuilders
 open Microsoft.Maui
 open Microsoft.Maui.Graphics
 open Microsoft.Maui.Accessibility
@@ -10,64 +12,114 @@ open Microsoft.Maui.Primitives
 open type Fabulous.Maui.View
 
 module App =
+    open Microsoft.Maui.Storage
+    open Microsoft.Maui.Media
+    open Microsoft.Maui.ApplicationModel
     type Model = {
-        Count: int
-        TrackedString: string option
+        TrackedString: string
+        Photos: FileResult list
      }
 
     type Msg =
-        | TrackedStringEntered
-        | TrackedStringRecognised
-        | Clicked
+        | TargetStringChanged of string
+        | TargetStringEntered
+        | TargetStringRecognised
+        | CapturePhotoClicked
+        | PhotoCaptured of FileResult option
 
-    type CmdMsg = SemanticAnnounce of string
+    type CmdMsg =
+        | CapturePhoto
+        // remove
+        | SemanticAnnounce of string
 
+    let ensureCameraPermissionAsync() =
+        task {
+            let! status = Permissions.CheckStatusAsync<Permissions.Camera>()
+            if status <> PermissionStatus.Granted then
+                if Permissions.ShouldShowRationale<Permissions.Camera>() then
+                    ()
+                let! newStatus = Permissions.RequestAsync<Permissions.Camera>()
+                return newStatus = PermissionStatus.Granted
+            else
+                return true
+        }
+
+    let capturePhoto =
+        task {
+            let! granted = ensureCameraPermissionAsync()
+            match granted with
+            | true ->
+                let! photo = MediaPicker.Default.CapturePhotoAsync()
+                let! stream = photo.OpenReadAsync()
+                let appDataDir = FileSystem.AppDataDirectory
+                let filePath = Path.Combine(appDataDir, photo.FileName)
+
+                use fileStream = File.Create filePath
+                do! stream.CopyToAsync fileStream
+                stream.Dispose()
+
+                return PhotoCaptured (Some photo)
+
+            | false -> return PhotoCaptured None
+
+        }
+        |> Cmd.ofTaskMsg
+            
+            
     let semanticAnnounce text =
         Cmd.ofSub(fun _ -> SemanticScreenReader.Announce(text))
 
     let mapCmd cmdMsg =
         match cmdMsg with
+        | CapturePhoto -> capturePhoto
+        // remove
         | SemanticAnnounce text -> semanticAnnounce text
 
-    let init () = { Count = 0; TrackedString = None }, []
+    let init () = { Photos = []; TrackedString = null }, []
 
     let update msg model =
         match msg with
-        | Clicked -> { model with Count = model.Count + 1 }, [ SemanticAnnounce $"Clicked {model.Count} times" ]
+        
+        | TargetStringChanged s -> model, [ SemanticAnnounce $"Clicked times" ]
+        | TargetStringEntered    -> model, [ SemanticAnnounce $"Clicked times" ]
+        | TargetStringRecognised -> model, [ SemanticAnnounce $"Clicked times" ]
+        
+        | CapturePhotoClicked -> model, [ CapturePhoto ]
+        | PhotoCaptured (Some photo) -> { model with Photos = photo :: model.Photos }, []
+        | PhotoCaptured None -> model, []
 
     let view model =
         Application(
             ContentPage(
-                ScrollView(
                     (VStack(spacing = 25.) {
-                        Image("dotnet_bot.png")
-                            .semantics(description = "Cute dotnet bot waving hi to you!")
-                            .height(200.)
-                            .centerHorizontal()
-
-                        Label("Hello, World!")
+                        Label("Enter the string to scan for")
                             .semantics(SemanticHeadingLevel.Level1)
-                            .font(size = 32.)
+                            .font(size = 24.)
                             .centerTextHorizontal()
-
-                        Label("Welcome to .NET Multi-platform App UI powered by Fabulous")
-                            .semantics(SemanticHeadingLevel.Level2, "Welcome to dot net Multi platform App U I powered by Fabulous")
-                            .font(size = 18.)
-                            .centerTextHorizontal()
-
-                        let text =
-                            if model.Count = 0 then
-                                "Click me"
-                            else
-                                $"Clicked {model.Count} times"
-
-                        Button(text, Clicked)
-                            .semantics(hint = "Counts the number of times you click")
                             .centerHorizontal()
+                        
+                        Label($"Photo set size: {model.Photos |> List.length}")
+                            .semantics(SemanticHeadingLevel.Level1)
+                            .font(size = 28.)
+                            .centerTextHorizontal()
+                            .centerHorizontal()
+
+                        Entry("",TargetStringChanged)
+                            .placeholder("Target string")
+                            .semantics(hint = "Enter the string to scan for")
+                            .font(size = 24.)
+                            .centerHorizontal()
+
+                        Button("Start Scan", TargetStringEntered)
+                            .semantics(hint = "Confirm when done")
+                            .centerHorizontal()
+
+                        Button("Capture Photo", CapturePhotoClicked)
+                            .semantics(hint = "Take a photo")
+                            .centerHorizontal()    
                     })
                         .padding(30., 0., 30., 0.)
                         .centerVertical()
-                )
             )
         )
 

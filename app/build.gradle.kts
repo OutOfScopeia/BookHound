8dd57c4671013df41ad054473248bdccb93674b0
Peter Dubec <peter.dubec@asseco-ce.com>
2026-09-22 18:02:35 +0200

    chore: bump versionCode to 2 and versionName to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.outofscopia.bookhound"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildFeatures {
